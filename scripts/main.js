aa66f39c6e8409ef7e4bc6cc4e7dcf865370b548
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-02-28 11:13:32 -0600

    Implement adaptive flex styling
    Co-authored-by: Sydney Sharp <Sydney-M-Sharp@users.noreply.github.com>
    Co-authored-by: William R. Stetar <soyuz43@users.noreply.github.com>
    Co-authored-by: Leah Sanders <lsand18@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-
+import { generateTileHtml } from "./moods.js";
 //setting constant variable 'container' equal to the HTML element of class = "tile-container"
 const container = document.querySelector('.tile-container')
 //modify innerHTML of container element
-
+container.innerHTML = generateTileHtml() //function
--- scripts/moods.js
+++ scripts/moods.js
@@ -1,8 +1,14 @@
 // * import database
+import { getMoods } from "./database.js"
+const emotions = getMoods()
 // * define function to format html
+export const generateTileHtml = () => {
+
 let htmlString = ``
     // iterate over each emotion object in the emotions array in the database
-    for (emotion of emotions) {
+for (const emotion of emotions) {
+
+
         //below is html components
         htmlString += `<section class="tile">
         <div class="tile_header"><h2>${emotion.name}</h2></div>
@@ -11,8 +17,7 @@ let htmlString = ``
             <div class="tile_advice-box_header">Macho Advice</div>                                                            
             <div class="tile_advice-box_content">${emotion.quotes}</div>
         </div>
-        </section>
+        </section>`
     }
-
-
-    
+    return htmlString
+    }
